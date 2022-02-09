13c24303fdf4277bc2f9595f58571a7e29952470
Oliver Forral <intrepion@gmail.com>
2022-02-08 21:47:27 -0700

    green

--- newtut/src/lib.rs
+++ newtut/src/lib.rs
@@ -1,11 +1,18 @@
 #[cfg(test)]
 mod get_folder_name_should {
+    use super::*;
+
     #[test]
     fn return_folder_name_with_app_name_hello_world_and_language_rust_and_program_type_library() {
         let expected = "a-hello-world-l-rust-p-library";
 
-        let actual = get_folder_name("hello world", "rust", "library");
+        let actual = &get_folder_name("hello world", "rust", "library");
 
         assert_eq!(actual, expected);
     }
+}
+
+pub fn get_folder_name<'a>(app_name: &'a str, language: &'a str, program_type: &'a str) -> String {
+    let app_name = app_name.replace(" ", "-");
+    format!("a-{app_name}-l-{language}-p-{program_type}")
 }
--- newtut/src/main.rs
+++ newtut/src/main.rs
@@ -1,3 +1,6 @@
+use newtut::get_folder_name;
+
 fn main() {
-    println!("Hello, world!");
+    let folder_name = get_folder_name("hello world", "rust", "library");
+    println!("{folder_name}");
 }
