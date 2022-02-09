1692e105d72629ffe0abb10fe62b619bf543becf
Oliver Forral <intrepion@gmail.com>
2022-02-09 04:40:32 -0700

    updating main

--- newtut/src/main.rs
+++ newtut/src/main.rs
@@ -1,6 +1,41 @@
 use newtut::get_folder_name;
+use std::{env, fs, process};
 
 fn main() {
-    let folder_name = get_folder_name("hello world", "rust", "library");
+    let usage = r#"Usage: newtut <username> <program-name> <language> <program-type>
+
+    <language> can only be the following:
+
+    - rust
+
+    <program-type> can only be the following:
+
+    - library
+"#;
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 5 {
+        println!("{usage}");
+
+        process::exit(1);
+    }
+
+    let user_name = &args[1];
+    let application_name = &args[2];
+    let language = &args[3];
+    let program_type = &args[4];
+
+    let path_name = format!("repos/github/{user_name}");
+
+    println!("mkdir -p path_name: {path_name}");
+
+    fs::create_dir_all(&path_name).expect("unable to create path name");
+
+    println!("cd {path_name}");
+
+    env::set_current_dir(&path_name).expect("changing directory failed");
+
+    let folder_name = get_folder_name(application_name, language, program_type);
     println!("{folder_name}");
 }
